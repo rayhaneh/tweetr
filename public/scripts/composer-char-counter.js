db58fb55edccdbd6217bea3e3d36e0b776616805
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-08 20:58:13 +0000

    Used jQuery to add a dynamic character counter for new tweets.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,25 @@
-let textElm    = $(".new-tweet textarea")
-let counterElm = textElm.closest("form").children('.counter')
+// This
 
-textElm.on("keyup",function (event) {
+// keyup/down/press handerlers will not work in case user copy/pastes using mouse (not keyboard).
+// Change/Blur handlers will not work because they only fire after the user deselects the input box.
 
-  counter = 140 - $(this).val().length
-  counterElm.text(counter)
+$(document).ready(function() {
 
-  if (counter < 0 && !counterElm.hasClass("negative")) {
-    counterElm.addClass("negative")
-  }
-  else if (counter >= 0 && counterElm.hasClass("negative")) {
-    counterElm.removeClass("negative")
-  }
+  let textElm    = $(".new-tweet textarea")
+  let counterElm = textElm.closest("form").children('.counter')
+
+
+  textElm.on("input",function (event) {
+    counter = 140 - $(this).val().length
+    counterElm.text(counter)
+
+    // Adding/removing the negative class to .counter
+    if (counter < 0 && !counterElm.hasClass("negative")) {
+      counterElm.addClass("negative")
+    }
+    else if (counter >= 0 && counterElm.hasClass("negative")) {
+      counterElm.removeClass("negative")
+    }
+  })
 
 })
-

edd2b9ddbb90d3a46ecd2ac475eefdfe6bd6d056
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-09 21:57:48 +0000

    Hide the error (if any) when the user starts typing. Added some comments for clarity.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,21 @@
-// This
+// This file
 
 // keyup/down/press handerlers will not work in case user copy/pastes using mouse (not keyboard).
 // Change/Blur handlers will not work because they only fire after the user deselects the input box.
 
 $(document).ready(function() {
 
-  let textElm    = $(".new-tweet textarea")
+  let textElm    = $("#new-tweet textarea")
   let counterElm = textElm.closest("form").children('.counter')
 
 
   textElm.on("input",function (event) {
     counter = 140 - $(this).val().length
     counterElm.text(counter)
+    $(".error").hide()
 
     // Adding/removing the negative class to .counter
+    // Checks if it already has that class to prevent re-adding!
     if (counter < 0 && !counterElm.hasClass("negative")) {
       counterElm.addClass("negative")
     }

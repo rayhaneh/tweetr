5f5b644bdbabe15506cc2e817826425df7596323
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-10 22:36:08 +0000

    Added a new DataHelper to update tweets after each like/unlike

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -4,7 +4,7 @@
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // SAVE TWEET HELPER
+    // SAVE A SINGLE TWEET
     saveTweet: function(newTweet, callback) {
       db.collection("tweets").insertOne(newTweet, (err, result) => {
         if (err) {
@@ -14,16 +14,38 @@ module.exports = function makeDataHelpers(db) {
       })
     },
 
-    // GET TWEET HELPER
+    // GET ALL TWEET
     getTweets: function(callback) {
-    db.collection("tweets").find().toArray((err, tweets) => {
-      if (err) {
-        return callback(err)
-      }
-      const sortNewestFirst = (a, b) => a.created_at - b.created_at
-      callback(null, tweets.sort(sortNewestFirst))
-    })
-  }
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err)
+        }
+        const sortNewestFirst = (a, b) => a.created_at - b.created_at
+        callback(null, tweets.sort(sortNewestFirst))
+      })
+    },
+
+
+    // UPDATES A SINGLE TWEET
+    updateTweet: function(filter,callback) {
+      db.collection("tweets").find(filter).toArray((err, tweet) => {
+        if (err) {
+          return callback(err)
+        }
+        tweet = tweet[0]
+        db.collection("tweets").updateOne(filter, { $set: {"like": !tweet.like}}, (err) => {
+            if(err) {
+              return callback(err)
+            }
+          })
+        callback(null)
+      })
+    }
+
+
+
+
+
 
 
   }

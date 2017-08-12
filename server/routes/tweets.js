bcb684d9c54857375c6f0598d6f2e94ce32f5183
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-12 15:51:26 +0000

    Modified the tweets/:id/like route to add a like counter to the database instead of a false nand true value.

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -34,12 +34,17 @@ module.exports = function(DataHelpers) {
       created_at: Date.now(),
       like: false
     }
-
     DataHelpers.getUser(filter, (err, user) => {
       if (err) {
         res.status(500).json({ error: err.message })
       } else {
-        tweet.user = user
+        tweet.user = {
+          name: user.name,
+          email: user.email,
+          avatars: user.avatars,
+          handle: user.handle
+        }
+        tweet.like = []
         DataHelpers.saveTweet(tweet, (err) => {
           if (err) {
             res.status(500).json({ error: err.message })
@@ -57,11 +62,28 @@ module.exports = function(DataHelpers) {
 
   tweetsRoutes.put("/:id/like", function(req, res) {
     const filter = { "_id": new Mongo.ObjectID(req.params.id)}
-    DataHelpers.updateTweet(filter, (err) => {
+    DataHelpers.getTweet(filter, (err, tweet) => {
       if (err) {
         res.status(500).json({ error: err.message })
       } else {
-        res.status(201).send()
+        if (tweet.like.indexOf(req.body.email) !== -1) {
+          DataHelpers.updateTweet(filter, {$pull: {like: req.body.email}}, (err) => {
+            if (err) {
+              res.status(500).json({ error: err.message })
+            } else {
+              res.status(201).send()
+            }
+          })
+        }
+        else {
+          DataHelpers.updateTweet(filter, {$push: {like: req.body.email}}, (err) => {
+            if (err) {
+              res.status(500).json({ error: err.message })
+            } else {
+              res.status(201).send()
+            }
+          })
+        }
       }
     })
   })

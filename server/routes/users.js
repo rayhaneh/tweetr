65ecfebaecee2d8dc755ce2942de3cfe3ddc74c3
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-12 15:44:59 +0000

    Added error messages for login fails. Added 'register a new user' feature to the code.

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,7 @@ const userHelper    = require("../lib/util/user-helper")
 const express       = require('express')
 const userRoutes  = express.Router()
 const bcrypt        = require('bcrypt')
+const md5 = require('md5')
 
 // const Mongo       = require("mongodb")
 
@@ -19,7 +20,7 @@ module.exports = function(DataHelpers) {
       if (err) {
         res.status(401).json({ error: err.message })
       } else if (!user) {
-        res.status(401).send("email does not exist")
+        res.send("Your email address is not registered.")
       }
       else {
         let pass = false
@@ -27,7 +28,7 @@ module.exports = function(DataHelpers) {
           pass = true
         }
         if (!pass ) {
-          res.status(401).send("wrong password")
+          res.send("The email address and password does not match.")
         }
         // req.session.user_id = req.body.email
         res.status(201).send()
@@ -35,26 +36,57 @@ module.exports = function(DataHelpers) {
     })
   }),
 
-  userRoutes.post("/logout", function(req,res) {
-    res.status(201).send()
-  }),
+  // userRoutes.post("/logout", function(req,res) {
+  //   res.status(201).send()
+  // }),
 
 
 
 
   userRoutes.post("/register", function(req,res) {
-    const user = {
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password,10),
-      name: req.body.name,
-      handle: req.body.handle
-    }
 
-    DataHelpers.saveUser(user, (err) => {
+    let filter = {email: req.body.email}
+    DataHelpers.getUser(filter, (err, user) => {
       if (err) {
-        res.status(500).json({ error: err.message })
-      } else {
-        res.status(201).send()
+        res.status(401).json({ error: err.message })
+      }
+      else if (user) {
+          return res.send('This email address is already registred.')
+      }
+      else {
+        filter = {handle: req.body.handle}
+        DataHelpers.getUser(filter, (err, user) => {
+          if (err) {
+            res.status(401).json({ error: err.message })
+          }
+          else if (user) {
+            return res.send('This handle is already registred.')
+          }
+          else {
+            const avatarUrlPrefix = `https://vanillicon.com/${md5(req.body.handle)}`
+            const avatars = {
+              small:   `${avatarUrlPrefix}_50.png`,
+              regular: `${avatarUrlPrefix}.png`,
+              large:   `${avatarUrlPrefix}_200.png`
+            }
+            const user = {
+              email: req.body.email,
+              password: bcrypt.hashSync(req.body.password,10),
+              name: req.body.name,
+              avatars: avatars,
+              handle: req.body.handle
+            }
+
+            DataHelpers.saveUser(user, (err) => {
+              if (err) {
+                res.status(500).json({ error: err.message })
+              } else {
+                res.status(201).send()
+              }
+            })
+
+          }
+        })
       }
     })
   })
